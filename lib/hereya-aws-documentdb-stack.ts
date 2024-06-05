de7e79c9efbe65aae3a533f1dd8124e145172550
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-06-05 22:35:06 +0200

    Update .gitignore and AWS DocumentDB Stack
    Added .idea and cdk.context.json to .gitignore and made substantial modifications to the hereya-aws-documentdb-stack.ts to use AWS DocumentDB. This restructured the code, added secrets manager and database cluster. Also implemented the synthesized stack for the AWS DocumentDB in hereya-aws-documentdb.ts and included a new package-lock.json file.

--- lib/hereya-aws-documentdb-stack.ts
+++ lib/hereya-aws-documentdb-stack.ts
@@ -1,16 +1,66 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import * as docdb from 'aws-cdk-lib/aws-docdb';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class HereyaAwsDocumentdbStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const vpc = Vpc.fromLookup(this, 'VPC', {
+            isDefault: true
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HereyaAwsDocumentdbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const cluster = new docdb.DatabaseCluster(this, 'Database', {
+            masterUser: {
+                username: 'myuser', // NOTE: 'admin' is reserved by DocumentDB
+                excludeCharacters: '\"@/:', // optional, defaults to the set "\"@/" and is also used for eventually created rotations
+            },
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MEDIUM),
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PUBLIC,
+            },
+            vpc,
+            copyTagsToSnapshot: false,  // whether to save the cluster tags when creating the snapshot.
+            removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+            parameterGroup: new docdb.ClusterParameterGroup(this, 'ParameterGroup', {
+                family: 'docdb5.0',
+                parameters: {
+                    'tls': 'disabled',
+                },
+            }),
+        });
+        cluster.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+
+        // Extract the secret details
+        const secret = cluster.secret!;
+        const username = secret.secretValueFromJson('username').unsafeUnwrap(); // Retrieve as string
+        const password = secret.secretValueFromJson('password').unsafeUnwrap(); // Retrieve as string
+        const host = cluster.clusterEndpoint.hostname;
+        const port = cluster.clusterEndpoint.port;
+        const replicaSetName = 'rs0'
+
+        // Format the MONGO_URL
+        const mongoUrl = `mongodb://${username}:${password}@${host}:${port}/mydb?replicaSet=${replicaSetName}&ssl=false`;
+
+
+        // Store the MONGO_URL in Secrets Manager
+        const mongoSecret = new secretsmanager.Secret(this, 'MongoDbConnectionStringSecret', {
+            secretStringValue: cdk.SecretValue.unsafePlainText(mongoUrl),
+            description: 'MongoDB connection string for DocumentDB cluster',
+        });
+
+        // Output the ARN of the secret
+        new cdk.CfnOutput(this, 'mongoUrl', {
+            value: mongoSecret.secretArn,
+        });
+
+        // export DB name
+        new cdk.CfnOutput(this, 'mongoDbname', {
+            value: 'mydb',
+        });
+    }
 }
