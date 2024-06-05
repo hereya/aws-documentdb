bb998a7e5809882b234398d6be977e8f199f6bf3
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-06-05 21:27:27 +0200

    Initial commit

---
+++ test/hereya-aws-documentdb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HereyaAwsDocumentdb from '../lib/hereya-aws-documentdb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hereya-aws-documentdb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HereyaAwsDocumentdb.HereyaAwsDocumentdbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
